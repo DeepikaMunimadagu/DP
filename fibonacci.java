d05991bd6856d3e81292e48a8011fd25eedc7d19
Deepika Chandrashekar Munimadagu <121370042+DeepikaMunimadagu@users.noreply.github.com>
2023-12-20 19:44:48 +0530

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -9,9 +9,20 @@ class Main{
         f[n] = fib(n-1,f) + fib(n-2,f);
         return f[n];
     }
+    public static int tabFib(int n){
+        int ans;
+        int dp[] = new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
     public static void main(String args[]){
         int n = 5;
         int f[] = new int[n+1];
         System.out.println(fib(n,f));
+        System.out.println(tabFib(n));
     }
 }
